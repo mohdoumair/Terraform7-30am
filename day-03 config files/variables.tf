19cf7b6997676c591401c5d60e77ee7a06eba854
oumair <mohdoumair197@gmail.com>
2025-07-10 20:23:25 +0530

    upadteday3

---
+++ day-03 config files/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_instance" {
+    type = string
+ default = ""
+
+
+}
+
+variable "instance_type" {
+  type = string
+default = ""
+
+}
+
+variable "aws_s3_bucket" {
+    type = string
+   default = ""  
+}
+  
