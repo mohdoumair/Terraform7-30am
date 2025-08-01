7c44878b38cdc956fb5e40c80a2a17921aee538e
mohdoumair <mohdoumair197@gmail.com>
2025-08-01 21:11:35 +0530

    Update main.tf

--- jenkins pratice/main.tf
+++ jenkins pratice/main.tf
@@ -1,6 +1,47 @@
-resource "aws_instance" "name" {
-    ami = "ami-0cbbe2c6a1bb2ad63"
-    instance_type = "t2.micro"
-    
-  
+pipeline {
+    agent any
+
+    parameters {
+        string(name: 'TF_ACTIONS', defaultValue: '', description: 'Optional Terraform actions or targets (e.g., -target=module.vpc)')
+    }
+
+    environment {
+        GIT_REPO_URL = 'https://github.com/mohdoumair/Terraform7-30am.git'
+        GIT_BRANCH   = 'main'
+        TF_DIR       = 'jenkins pratice'
+    }
+
+    stages {
+        stage('Git Clone') {
+            steps {
+                git branch: "${GIT_BRANCH}", url: "${GIT_REPO_URL}"
+            }
+        }
+
+        stage('Terraform Init') {
+            steps {
+                dir("${TF_DIR}") {
+                    sh 'terraform init'
+                }
+            }
+        }
+
+        stage('Terraform Plan') {
+            steps {
+                dir("${TF_DIR}") {
+                    sh 'terraform plan'
+                }
+            }
+        }
+
+        stage('Terraform Apply') {
+            steps {
+                dir("${TF_DIR}") {
+                    sh "terraform apply ${params.TF_ACTIONS} -auto-approve"
+                }
+            }
+        }
+    }
 }
+
+
